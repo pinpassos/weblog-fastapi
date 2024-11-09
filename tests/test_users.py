e2c878198e5515611e3f4a05cf9fd6501e9bbf7a
Matheus Pinheiro Passos <pinheiropassosm@gmail.com>
2024-11-09 18:05:52 -0300

    tests: Added tests to users model

---
+++ tests/test_users.py
@@ -0,0 +1,18 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
+
+
+def test_create_new_user(session, user):
+    """Ensure user can be createad."""
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    assert user.id == 1
+
+
+def test_if_user_has_unique_constraint(session, user, duplicated_user):
+    """Ensure that there is only one user per username"""
+    session.add_all([user, duplicated_user])
+    with pytest.raises(IntegrityError):
+        session.commit()

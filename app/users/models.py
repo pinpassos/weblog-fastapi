ed41870450b54f48abf1e195ce1914170e73b320
Matheus Pinheiro Passos <pinheiropassosm@gmail.com>
2024-11-10 15:06:20 -0300

    feat: Added posts to user model

--- app/users/models.py
+++ app/users/models.py
@@ -1,17 +1,22 @@
+from datetime import datetime
+from typing import List
+
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy import DateTime, Integer, String, func
+from sqlalchemy import String, func
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from app.posts.models import Post
 from app.settings.database import Base, async_session
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    created_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), nullable=False)
-    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    posts: Mapped[List["Post"]] = relationship(back_populates="author")
+    created_at: Mapped[datetime] = mapped_column(default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now(), nullable=False)
 
 
 async def get_user_db(session: AsyncSession = Depends(async_session)):

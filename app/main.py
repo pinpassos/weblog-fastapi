57c78310f10bc90b85acbec1b754283d12db9cce
Matheus Pinheiro Passos <pinheiropassosm@gmail.com>
2024-11-14 18:50:49 -0300

    feat: Added router to category

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from .healthcheck import healthcheck_router
-from .posts.routers import post_router
+from .posts.routers import post_router, categories_router
 from .users.routers import user_router
 
 app = FastAPI(title="Weblog - Back-end")
@@ -9,3 +9,4 @@ app = FastAPI(title="Weblog - Back-end")
 app.include_router(router=healthcheck_router,prefix="/healthcheck", include_in_schema=False)
 app.include_router(router=user_router, prefix="/users", include_in_schema=True)
 app.include_router(router=post_router, prefix="/posts", include_in_schema=True)
+app.include_router(router=categories_router, prefix="/categories", include_in_schema=True)
